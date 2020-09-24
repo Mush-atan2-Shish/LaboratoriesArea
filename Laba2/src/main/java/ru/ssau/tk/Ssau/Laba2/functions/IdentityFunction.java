06824d56de1f240022bbc855c474088dcc337b22
Shishova <shishova.alena2013@yandex.ru>
2020-09-24 20:40:24 +0300

    Добавлен класс IdentityFunction и тесты для него

---
+++ Laba2/src/main/java/ru/ssau/tk/Ssau/Laba2/functions/IdentityFunction.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.Ssau.Laba2.functions;
+
+public class IdentityFunction implements MathFunction{
+
+
+    @Override
+    public double apply(double x) {
+        return x;
+    }
+}
---
+++ Laba2/src/test/java/ru/ssau/tk/Ssau/Laba2/functions/IdentityFunctionTest.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.Ssau.Laba2.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class IdentityFunctionTest {
+
+
+    @Test
+    public void testApply() {
+        MathFunction function = new IdentityFunction();
+        assertEquals(function.apply(1), 1);
+        assertEquals(function.apply(0), 0);
+        assertEquals(function.apply(1.55), 1.55, 0.0001);
+        assertEquals(function.apply(-2.15), -2.15, 0.001);
+
+    }
+}
