313a74421c42009fdcf959381cef861d919410f8
Shishova <shishova.alena2013@yandex.ru>
2020-11-11 17:51:42 +0300

    3.2 X:Добавить в класс TabulatedFunctionOperationService методы умножения и деления двух функций по аналогии с тем, как это сделано для сложения и вычитания. Аналогично покрыть методы тестами.

--- Laba2/src/test/java/ru/ssau/tk/Ssau/Laba2/operations/TabulatedFunctionOperationServiceTest.java
+++ Laba2/src/test/java/ru/ssau/tk/Ssau/Laba2/operations/TabulatedFunctionOperationServiceTest.java
@@ -8,6 +8,7 @@ import ru.ssau.tk.Ssau.Laba2.functions.Point;
 import ru.ssau.tk.Ssau.Laba2.functions.TabulatedFunction;
 import ru.ssau.tk.Ssau.Laba2.functions.factory.ArrayTabulatedFunctionFactory;
 import ru.ssau.tk.Ssau.Laba2.functions.factory.LinkedListTabulatedFunctionFactory;
+import ru.ssau.tk.Ssau.Laba2.functions.factory.TabulatedFunctionFactory;
 
 import static org.testng.Assert.*;
 
@@ -117,6 +118,68 @@ public class TabulatedFunctionOperationServiceTest {
         for (Point point : testSubtractOfArrayAndList) {
             assertEquals(point.x, valuesX[i]);
             assertEquals(point.y, valuesY[i] - valuesYForList[i++]);
+        }
+    }
+
+    @Test
+    public void testMultiply() {
+        double[] yValues1 = new double[]{2., 8., 32., 160.};
+        double[] xValues1 = new double[]{2., 4., 6., 8.};
+        double[] yValues2 = new double[]{20., 40., 60., 80.};
+        double[] xValues2 = new double[]{2., 4., 6., 8.};
+
+        TabulatedFunctionFactory linkedListFactory = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunction a = new ArrayTabulatedFunctionFactory().create(xValues1, yValues1);
+        TabulatedFunction b = linkedListFactory.create(xValues2, yValues2);
+        TabulatedFunctionOperationService operationService = new TabulatedFunctionOperationService();
+        TabulatedFunction multiplyThroughArray = operationService.multiply(a, a);
+        TabulatedFunction multiplyThroughLinkedList = operationService.multiply(b, b);
+        TabulatedFunction multiplyOfArrayAndList = operationService.multiply(a, b);
+        int i = 0;
+        for (Point point : multiplyThroughArray) {
+            assertEquals(point.x, xValues1[i]);
+            assertEquals(point.y, yValues1[i] * yValues1[i++]);
+        }
+        i = 0;
+        for (Point point : multiplyThroughLinkedList) {
+            assertEquals(point.x, xValues2[i]);
+            assertEquals(point.y, yValues2[i] * yValues2[i++]);
+        }
+        i = 0;
+        for (Point point : multiplyOfArrayAndList) {
+            assertEquals(point.x, xValues1[i]);
+            assertEquals(point.y, yValues1[i] * yValues2[i++]);
+        }
+    }
+
+    @Test
+    public void testDivide() {
+        double[] yValues1 = new double[]{2., 8., 32., 160.};
+        double[] xValues1 = new double[]{2., 4., 6., 8.};
+        double[] yValues2 = new double[]{20., 40., 60., 80.};
+        double[] xValues2 = new double[]{2., 4., 6., 8.};
+
+        TabulatedFunctionFactory linkedListFactory = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunction a = new ArrayTabulatedFunctionFactory().create(xValues1, yValues1);
+        TabulatedFunction b = linkedListFactory.create(xValues2, yValues2);
+        TabulatedFunctionOperationService operationService = new TabulatedFunctionOperationService();
+        TabulatedFunction divideThroughArray = operationService.divide(a, a);
+        TabulatedFunction divideThroughLinkedList = operationService.divide(b, b);
+        TabulatedFunction divideArrayAndList = operationService.divide(a, b);
+        int i = 0;
+        for (Point point : divideThroughArray) {
+            assertEquals(point.x, xValues1[i]);
+            assertEquals(point.y, yValues1[i] / yValues1[i++]);
+        }
+        i = 0;
+        for (Point point : divideThroughLinkedList) {
+            assertEquals(point.x, xValues2[i]);
+            assertEquals(point.y, yValues2[i] / yValues2[i++]);
+        }
+        i = 0;
+        for (Point point : divideArrayAndList) {
+            assertEquals(point.x, xValues1[i]);
+            assertEquals(point.y, yValues1[i] / yValues2[i++]);
         }
     }
 }
