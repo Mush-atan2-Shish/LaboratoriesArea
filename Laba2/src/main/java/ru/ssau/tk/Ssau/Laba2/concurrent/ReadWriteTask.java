a51efaad0342dfbb0351d8feb991ea67d2c2fab8
Shishova <shishova.alena2013@yandex.ru>
2020-11-25 14:13:49 +0300

    4.1 X:Рядом с пакетами exceptions, functions, io и operations создать пакет concurrent.       В пакете concurrent создать класс ReadWriteTask, реализующий интерфейс Runnable. Класс должен иметь приватное поле, содержащее ссылку на табулированную функцию, которая передаётся в конструкторе.       Реализовать метод run() следующим образом:       1) В цикле for (не for-each) пробежаться по всем записям табулированной функции. Все следующие пункты относятся к телу цикла.       2) Получить значение x по индексу и записать в переменную.       3) Получить значение y по индексу и записать в переменную.       4) Вывести в консоль информацию:       %s, before write: i = %d, x = %f, y = %f       5) С помощью метода setY() изменить значение функции на y + 1.       6) Вновь получить значение y по индексу и записать в переменную.       7) Вывести в консоль информацию:       %s, after write: i = %d, x = %f, y = %f       В параметры обеих строк должны быть подставлены соответственно: название текущего потока, индекс и значения (x, y) из табулированной функции.

---
+++ Laba2/src/main/java/ru/ssau/tk/Ssau/Laba2/concurrent/ReadWriteTask.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.Ssau.Laba2.concurrent;
+
+import ru.ssau.tk.Ssau.Laba2.functions.TabulatedFunction;
+
+public class ReadWriteTask implements Runnable {
+    public ReadWriteTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    private final TabulatedFunction function;
+
+    @Override
+    public void run() {
+        double x;
+        double y;
+        for (int i = 0; i < function.getCount(); i++) {
+            x = function.getX(i);
+            y = function.getY(i);
+            System.out.printf("%s, before write: i = %d, x = %f, y = %f \n", Thread.currentThread().getName(), i, x, y);
+            function.setY(i, y + 1);
+            y = function.getY(i);
+            System.out.printf("%s, after write: i = %d, x = %f, y = %f \n", Thread.currentThread().getName(), i, x, y);
+        }
+    }
+}
