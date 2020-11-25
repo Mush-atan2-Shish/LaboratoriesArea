24e3a95f7912ed6b3f8279ce3f38b3d4502f4ca4
Shishova <shishova.alena2013@yandex.ru>
2020-11-25 16:30:09 +0300

    4.1 X:Thread-0, before write: i = 0, x = 1,000000, y = 0,000000       Thread-19, before write: i = 0, x = 1,000000, y = 0,000000       Thread-18, before write: i = 0, x = 1,000000, y = 0,000000       Thread-18, after write: i = 0, x = 1,000000, y = 1,000000 В пакете concurrent создать класс ReadWriteTaskExecutor с main-методом. Внутри метода должна создаваться табулированная функция (с реализацией в виде связного списка) с помощью конструктора, который создаёт функцию на основе другой функции. В конструктор требуется передать тождественно равную нулю функцию ZeroFunction и интервал от 1 до 10. Столько же (10) точек должна иметь функция. Далее требуется создать двадцать потоков исполнения Thread, каждый из которых принимает объект ReadWriteTask в конструкторе. При создании объекта следует передать в его конструктор ранее созданную табулированную функцию. Все созданные потоки должны быть добавлены в коллекцию-список List<Thread>. После создания потоков требуется их стартовать (в отдельном цикле, пробежавшись по списку потоков с помощью цикла for-each). Затем необходимо усыпить текущий поток (например, на пару секунд), дав шанс выполниться остальным. После пробуждения текущий поток должен вывести в консоль табулированную функцию. Что же должно получиться после запуска? Если бы потоки выполнялись последовательно, то каждый поток по очереди выполнил бы одну и ту же задачу, прибавив единицу к каждому значению табулированной функции. Так как на входе значения равнялись нулю (ZeroFunction), то на выходе бы получилась функция, тождественно равная 20 (так как было 20 потоков). Запустить метод на исполнение. Изучить выведенную в консоль информацию. С большой вероятностью значения таблицы не будут доходить до 20. Это связно с тем, что иногда несколько потоков одновременно берут значения из функции, а затем записывают новое, не проверяя, не успел ли поменять значение y кто-то ещё. Найти в консоли примеры таких случаев, когда записывалось неправильное значение.

---
+++ Laba2/src/main/java/ru/ssau/tk/Ssau/Laba2/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.Ssau.Laba2.concurrent;
+
+import ru.ssau.tk.Ssau.Laba2.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.Ssau.Laba2.functions.TabulatedFunction;
+import ru.ssau.tk.Ssau.Laba2.functions.ZeroFunction;
+
+import java.util.ArrayList;
+
+public class ReadWriteTaskExecutor {
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction function = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
+        ArrayList<Thread> list = new ArrayList<>();
+        ReadWriteTask myTask = new ReadWriteTask(function);
+        for (int i = 0; i < 20; i++){
+            list.add(new Thread(myTask));
+        }
+        for (Thread thread: list){
+            thread.start();
+        }
+        Thread.sleep(2_000);
+        System.out.println(function.toString());
+    }
+}
