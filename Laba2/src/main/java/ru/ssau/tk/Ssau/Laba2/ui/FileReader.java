6949b7a1e013e8be0d9aa05f141a44f434b92483
Mushankova <karina-mush21@mail.ru>
2020-12-24 15:49:00 +0400

    Изменены классы FileReader и FileWriter

--- Laba2/src/main/java/ru/ssau/tk/Ssau/Laba2/ui/FileReader.java
+++ Laba2/src/main/java/ru/ssau/tk/Ssau/Laba2/ui/FileReader.java
@@ -14,66 +14,33 @@ import java.io.FileInputStream;
 import java.util.function.Consumer;
 
 public class FileReader extends JDialog {
-    private JTextField filename = new JTextField();
-    private JTextField directory = new JTextField();
-    private JButton download = new JButton("Выбрать файл");
     private TabulatedFunction function;
     private TabulatedFunctionFactory factory;
 
     public FileReader(Consumer<? super TabulatedFunction> callback) {
-        setTitle("Открыть функцию");
         setModal(true);
-        JPanel panel = new JPanel();
-        addListenerForOpenButton(callback);
-        download.setBackground(Color.pink);
-        download.setFocusPainted(false);
-        panel.add(download);
-        Container pane = getContentPane();
-        pane.add(panel, BorderLayout.SOUTH);
-        directory.setEditable(false);
-        filename.setEditable(false);
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 1));
-        panel.add(filename);
-        panel.add(directory);
-        pane.add(panel, BorderLayout.NORTH);
-    }
-
-    public void addListenerForOpenButton(Consumer<? super TabulatedFunction> callback) {
-        download.addActionListener(event -> {
-            JFileChooser chooser = new JFileChooser();
-            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            chooser.addChoosableFileFilter(
-                    new FileNameExtensionFilter("Bin files", "bin"));
-            chooser.setAcceptAllFileFilterUsed(false);
-            int rVal = chooser.showOpenDialog(FileReader.this);
-            if (rVal == JFileChooser.APPROVE_OPTION) {
-                filename.setText(chooser.getSelectedFile().getName());
-                directory.setText(chooser.getCurrentDirectory().toString());
-                File file = chooser.getSelectedFile();
-                factory = new ArrayTabulatedFunctionFactory();
-                if (file != null) {
-                    try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
-                        function = FunctionsIO.readTabulatedFunction(inputStream, factory);
-                        callback.accept(function);
-                    } catch (Exception e) {
-                        new ErrorsWindow(this, e);
-                    }
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        chooser.addChoosableFileFilter(
+                new FileNameExtensionFilter("Bin files", "bin"));
+        chooser.setAcceptAllFileFilterUsed(false);
+        int rVal = chooser.showOpenDialog(FileReader.this);
+        if (rVal == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            factory = new ArrayTabulatedFunctionFactory();
+            if (file != null) {
+                try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
+                    function = FunctionsIO.readTabulatedFunction(inputStream, factory);
+                    callback.accept(function);
+                } catch (Exception e) {
+                    new ErrorsWindow(this, e);
                 }
             }
-            if (rVal == JFileChooser.CANCEL_OPTION) {
-                filename.setText("Отмена");
-                directory.setText("");
-            }
-        });
+        }
+
     }
 
     public static void main(Consumer<? super TabulatedFunction> callback) {
-        run(new FileReader(callback), 250, 110);
-    }
-
-    public static void run(JDialog frame, int width, int height) {
-        frame.setSize(width, height);
-        frame.setVisible(true);
+        new FileReader(callback);
     }
 }
--- Laba2/src/main/java/ru/ssau/tk/Ssau/Laba2/ui/FileWriter.java
+++ Laba2/src/main/java/ru/ssau/tk/Ssau/Laba2/ui/FileWriter.java
@@ -11,64 +11,30 @@ import java.io.File;
 import java.io.FileOutputStream;
 
 public class FileWriter extends JDialog {
-    private JTextField filename = new JTextField();
-    private JTextField directory = new JTextField();
-    private JButton save = new JButton("Сохранить функцию");
     private TabulatedFunction function;
 
     public FileWriter(TabulatedFunction func) {
-        setTitle("Сохранить функцию");
         setModal(true);
         this.function = func;
-        JPanel panel = new JPanel();
-        addListenerForSaveButton();
-        save.setFocusPainted(false);
-        save.setBackground(Color.pink);
-        panel.add(save);
-        Container cp = getContentPane();
-        cp.add(panel, BorderLayout.SOUTH);
-        directory.setEditable(false);
-        filename.setEditable(false);
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 1));
-        panel.add(filename);
-        panel.add(directory);
-        cp.add(panel, BorderLayout.NORTH);
-    }
-
-    public void addListenerForSaveButton() {
-        save.addActionListener(event -> {
-            JFileChooser chooser = new JFileChooser();
-            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            chooser.addChoosableFileFilter(
-                    new FileNameExtensionFilter("Bin files", "bin"));
-            chooser.setAcceptAllFileFilterUsed(false);
-            int rVal = chooser.showSaveDialog(FileWriter.this);
-            if (rVal == JFileChooser.APPROVE_OPTION) {
-                filename.setText(chooser.getSelectedFile().getName());
-                directory.setText(chooser.getCurrentDirectory().toString());
-                File file = chooser.getSelectedFile();
-                if (file != null) {
-                    try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
-                        FunctionsIO.writeTabulatedFunction(outputStream, function);
-                    } catch (Exception e) {
-                        new ErrorsWindow(this, e);
-                    }
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        chooser.addChoosableFileFilter(
+                new FileNameExtensionFilter("Bin files", "bin"));
+        chooser.setAcceptAllFileFilterUsed(false);
+        int rVal = chooser.showSaveDialog(FileWriter.this);
+        if (rVal == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            if (file != null) {
+                try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
+                    FunctionsIO.writeTabulatedFunction(outputStream, function);
+                } catch (Exception e) {
+                    new ErrorsWindow(this, e);
                 }
             }
-            if (rVal == JFileChooser.CANCEL_OPTION) {
-                filename.setText("Отмена");
-                directory.setText("");
-            }
-        });
+        }
     }
 
     public static void main(TabulatedFunction func) {
-        run(new FileWriter(func), 250, 110);
-    }
-
-    public static void run(JDialog frame, int width, int height) {
-        frame.setSize(width, height);
-        frame.setVisible(true);
+        new FileWriter(func);
     }
 }
